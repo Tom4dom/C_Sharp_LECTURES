e905f76c29b51a9232b979ca8ccfb0b2b8aee806
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2022-12-13 11:11:44 +0300

    added random

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,5 +1,8 @@
-﻿int numberA = 9;
-int numberB = 5;
-int numberC = 1;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 5 .... 9 [min, max-1]
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 10);
+Console.WriteLine(numberC);
 int result = numberA + numberB + numberC;
 Console.WriteLine(result);
