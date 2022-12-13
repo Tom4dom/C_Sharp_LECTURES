3df0ffaaf180bb7cdfdc44d4713c778b9125feb7
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2022-12-13 13:27:03 +0300

    Example_005_IfElse condition

---
+++ Example_0005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura! It is Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
