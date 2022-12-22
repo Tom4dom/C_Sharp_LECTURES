18e5f3749ce9b9993601edb12086d852822b009a
Tom4dom <120015368+Tom4dom@users.noreply.github.com>
2022-12-22 07:33:15 +0300

    Example_010_was_added

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 45, 12, 45, 45, 45, 8845, 345, 2432, 45, 435, 45, 45, 45, 45, 45 };
+
+int n = array.Length;
+int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+ 
+    index++;
+}
